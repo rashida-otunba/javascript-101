c281990b557e1f8a8042e5c62b3830026e2bc97b
rashida-otunba <rashidaotunba@gmail.com>
2021-12-26 06:30:55 -0500

    First push to git - JS 101

---
+++ recipe-exercises/script.js
@@ -0,0 +1,16 @@
+function marinaraSauce(cookName){
+    let two = 2;
+    let oneFourth = "1/4";
+    console.log(`hey ${cookName}!This is my marinara sauce recipe! `);
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log("- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log("- Add " + oneFourth + " cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Adam");
+
+marinaraSauce();
+
+
---
+++ script.js
@@ -0,0 +1,45 @@
+
+// const firstNumber=23;
+// // let secondNumber=19;
+
+
+// let secondNumber=108;
+
+
+// let theAnswer= firstNumber + secondNumber;
+
+// secondNumber=9000;
+
+// theAnswer= firstNumber + secondNumber;
+
+// console.log(theAnswer);
+
+// console.log(`theAnswer is now ${theAnswer}`);
+
+function add(num1, num2){
+    //your code here 
+    let theResult=num1+num2;
+    console.log(theResult);
+}
+
+add(2,3);
+
+
+
+
+function subtract(num1,num2){
+    let result = num2-num1;
+    console.log(result);
+  
+}
+
+subtract(9, 20); 
+
+function mySum(){
+    let result = add+subtract; 
+    console.log(result);
+}
+mySum();
+
+
+
